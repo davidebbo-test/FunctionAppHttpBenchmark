60449eaa86a7d3a049bf8a0de574084b1621e21c
David Ebbo <david.ebbo@gmail.com>
2017-11-07 14:00:30 -0800

    Add matrix multiplication scenario

--- FunctionAppHttpBenchmark/HttpTest.cs
+++ FunctionAppHttpBenchmark/HttpTest.cs
@@ -19,6 +19,7 @@ namespace FunctionAppHttpBenchmark
             long memory = req.GetLongQueryStringParam("AllocInMB", 0);
             int sleep = (int)req.GetLongQueryStringParam("SleepInMS", 0);
             long iterations = req.GetLongQueryStringParam("LoopSpins", 0);
+            int matrixSize = (int)req.GetLongQueryStringParam("MatrixSize", 0);
             var rnd = new Random();
 
             // Allocate memory and fill it with random bytes
@@ -30,6 +31,12 @@ namespace FunctionAppHttpBenchmark
 
             // Spin the CPU
             for (long i = 0; i < iterations; i++) { }
+
+            // Do matrix multiplications
+            if (matrixSize > 0)
+            {
+                Matrix.DoMatrixMultiplication(matrixSize);
+            }
 
             var response = req.CreateResponse(HttpStatusCode.OK, "Done");
             response.Headers.Add("X-server", Environment.GetEnvironmentVariable("COMPUTERNAME"));
---
+++ FunctionAppHttpBenchmark/Matrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAppHttpBenchmark
+{
+    // Code copied from https://github.com/Azure/azure-functions-performance-test/blob/dev/SampleUsages/TestSamples/Http-CSharp-CPUIntensive/run.csx
+    class Matrix
+    {
+        public static void DoMatrixMultiplication(int size)
+        {
+            int seed = 123;
+            int valueMin = 0;
+            int valueMax = 101;
+
+            int[][] matrix = CreateRandomMatrix(size, seed, valueMin, valueMax);
+            seed = 2 * seed;
+            int[][] matrix2 = CreateRandomMatrix(size, seed, valueMin, valueMax);
+            int[][] result = MultiplyMatrix(matrix, matrix2);
+        }
+
+        public static int[][] CreateRandomMatrix(int size, int seed, int valueMin, int valueMax)
+        {
+            Random rng = new Random(seed);
+            var matrix = new int[size][];
+            for (var i = 0; i < size; i++)
+            {
+                var row = new int[size];
+                for (var j = 0; j < size; j++)
+                {
+                    row[j] = rng.Next(valueMin, valueMax);
+                }
+
+                matrix[i] = row;
+            }
+            return matrix;
+        }
+
+        public static int[][] MultiplyMatrix(int[][] matrixA, int[][] matrixB)
+        {
+            int[][] result = new int[matrixA.GetLength(0)][];
+            int elements = matrixB.GetLength(0);
+            for (var i = 0; i < matrixA.GetLength(0); i++)
+            {
+                result[i] = new int[elements];
+                for (var j = 0; j < matrixA.GetLength(0); j++)
+                {
+                    var row = matrixA[i];
+                    var col = GetColumn(matrixB, j);
+
+                    result[i][j] = MultiplyRowAndColumn(row, col);
+                }
+            }
+            return result;
+        }
+
+        private static int MultiplyRowAndColumn(int[] row, int[] col)
+        {
+            int sum = 0;
+            for (int b = 0; b < row.Length; b++)
+            {
+                sum += row[b] * col[b];
+            }
+
+            return sum;
+        }
+
+        private static int[] GetColumn(int[][] matrixB, int j)
+        {
+            var result = new int[matrixB.Length];
+            for (int i = 0; i < matrixB.Length; i++)
+            {
+                result[i] = matrixB[i][j];
+            }
+
+            return result;
+        }
+    }
+}
