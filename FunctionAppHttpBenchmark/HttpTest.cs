c477711cab56289bd597717b4dcbe40b1bee4ed9
Mathew Charles <mathew.charles@microsoft.com>
2017-11-14 17:06:34 -0800

    Adding outbound connections scenario

--- FunctionAppHttpBenchmark/HttpTest.cs
+++ FunctionAppHttpBenchmark/HttpTest.cs
@@ -6,11 +6,14 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using System;
+using System.Collections.Generic;
 
 namespace FunctionAppHttpBenchmark
 {
     public static class HttpTest
     {
+        private static HttpClient client = new HttpClient();
+
         [FunctionName("HttpTest")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
@@ -18,6 +21,7 @@ namespace FunctionAppHttpBenchmark
             int sleep = (int)req.GetLongQueryStringParam("SleepInMS", 0);
             long iterations = req.GetLongQueryStringParam("LoopSpins", 0);
             int matrixSize = (int)req.GetLongQueryStringParam("MatrixSize", 0);
+            int requests = (int)req.GetLongQueryStringParam("Requests", 0);
             var rnd = new Random();
 
             // Allocate memory and fill it with random bytes
@@ -34,6 +38,17 @@ namespace FunctionAppHttpBenchmark
             if (matrixSize > 0)
             {
                 Matrix.DoMatrixMultiplication(matrixSize);
+            }
+
+            // Make some outbound http requests in parallel
+            if (requests > 0)
+            {
+                var tasks = new List<Task>();
+                for (int i = 0; i < requests; i++)
+                {
+                    tasks.Add(client.GetAsync("http://microsoft.com"));
+                }
+                await Task.WhenAll(tasks);
             }
 
             var response = req.CreateResponse(HttpStatusCode.OK, "Done");
